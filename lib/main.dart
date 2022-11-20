c641e6cc2fdfd797a64ca6b4caa12b04291880ab
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-11-20 14:38:30 -0300

    style: order result

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,28 @@ class _PerguntaAppState extends State<PerguntaApp> {
     {
       'texto': 'Qual é a sua cor favorita?',
       'respostas': [
-        {'texto': 'Preto', 'pontuacao': 10}, 
-        {'texto': 'Vermelho', 'pontuacao': 5}, 
-        {'texto': 'Verde', 'pontuacao': 3}, 
-        {'texto': 'Branco', 'pontuacao': 1}
+        {'texto': 'Preto', 'pontuacao': 2}, 
+        {'texto': 'Vermelho', 'pontuacao': 4}, 
+        {'texto': 'Branco', 'pontuacao': 6},
+        {'texto': 'Verde', 'pontuacao': 10}, 
         ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
       'respostas': [
-        {'texto': 'Coelho', 'pontuacao': 10}, 
-        {'texto': 'Cobra', 'pontuacao': 7}, 
-        {'texto': 'Panda', 'pontuacao': 8}, 
-        {'texto': 'Elefante', 'pontuacao': 10}
+        {'texto': 'Coelho', 'pontuacao': 2}, 
+        {'texto': 'Cobra', 'pontuacao': 4}, 
+        {'texto': 'Elefante', 'pontuacao': 6},
+        {'texto': 'Panda', 'pontuacao': 10}, 
         ],
     },
     {
       'texto': 'Qual é o seu instrutor favorito?',
       'respostas': [
-        {'texto': 'Maria', 'pontuacao': 4}, 
-        {'texto': 'João', 'pontuacao': 9}, 
+        {'texto': 'Maria', 'pontuacao': 2}, 
+        {'texto': 'João', 'pontuacao': 3}, 
+        {'texto': 'Pedro', 'pontuacao': 3},
         {'texto': 'Leo', 'pontuacao': 10}, 
-        {'texto': 'Pedro', 'pontuacao': 9}
         ],
     },
   ].cast();
