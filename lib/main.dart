5cba1921909620a5dc71e27f169a273c821dcc80
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-11-20 14:18:18 -0300

    feat: added dynamic response

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
             perguntaSelecionada: _perguntaSelecionada, 
             quandoResponder: _responder
             )
-          : Resultado(),
+          : Resultado(_pontuacaoTotal),
       )
     );
   }
